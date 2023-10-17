d3e31d9bff8e0a8f97db0eda5a4e6816e12c7c2c
maxiboy <maxiboyx@gmail.com>
2023-10-17 18:29:31 +0300

    bitti

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,10 @@
+package Giris;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        int m =  -12;
+        int n =  -5;
+        m %= n;
+        System.out.print(m);
+    }
+}
